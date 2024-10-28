bafac613f74c3134d6984b3c401aae8ee4385778
tetraphoz <tetraphosphorus@gmail.com>
2024-10-28 02:20:34 -0600

    Cantidad de casillas dinamicas y mejora de gui

--- main.py
+++ main.py
@@ -8,51 +8,55 @@ from kivy.uix.spinner import Spinner
 from kivy.uix.popup import Popup
 from kivy.animation import Animation
 from kivy.uix.label import Label
+import sys
+
+
+# CASILLAS = int(sys.argv[1])
+CASILLAS = 10
 
 
 class Search(GridLayout):
     def __init__(self, **kwargs):
         super(Search, self).__init__(**kwargs)
-        self.cols = 10
-        self.rows = 10
+        self.cols = CASILLAS
+        self.rows = CASILLAS
         self.spacing = 5
         self.padding = 10
         self.buttons = []  # Estructura de datos para interactuar con botones
 
         # Generamos cuadricula
-        for i in range(1, 101):
+        for i in range(1, CASILLAS ** 2 + 1):
             button = CustomButton()
             if i == 1:
                 button.background_color = "darkseagreen"
+                button.background_normal = ''
                 button.is_toggeable = False
-            if i == 100:
+            if i == CASILLAS ** 2:
                 button.background_color = "maroon"
+                button.background_normal = ''
                 button.is_toggeable = False
             self.buttons.append(button)
             self.add_widget(button)
 
     def get_neighbours(self, index):
         neighbors = []
-        row, col = index // 10, index % 10
+        row, col = index // CASILLAS, index % CASILLAS
 
         # Check all 4 directions: right, down, left, up
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
         for dr, dc in directions:
             new_row, new_col = row + dr, col + dc
-            if (0 <= new_row < 10 and 0 <= new_col < 10):
-                neighbor_index = new_row * 10 + new_col
+            if (0 <= new_row < CASILLAS and 0 <= new_col < CASILLAS):
+                neighbor_index = new_row * CASILLAS + new_col
                 if not self.buttons[neighbor_index].is_toggled:
                     neighbors.append(neighbor_index)
 
         return neighbors
 
     def dfs(self):
-        start = 0  # Button 1
-        end = 99   # Button 100
-
-        if self.buttons[start].is_toggled or self.buttons[end].is_toggled:
-            return None
+        start = 0
+        end = CASILLAS ** 2 - 1
 
         stack = [(start, [start])]
         visited = set()
@@ -94,44 +98,71 @@ class MainLayout(BoxLayout):
         button_layout = BoxLayout(size_hint=(1, 0.1))
 
         self.spinner = Spinner(text='DFS',
-                          values=('DFS', 'BFS', 'A*'))
+                               font_size = self.width / 2,
+                               background_normal='',
+                               color= 'black',
+                               background_color = 'lavender',
+                               values=('DFS', 'BFS', 'A*'))
 
-        btn_buscar = Button(text="Buscar")
-        btn_buscar.background_color = "coral"
-        btn_buscar.bind(on_press=self.buscar)
+        self.btn_buscar = Button(text="Buscar",
+                                 font_size = self.width / 2,
+                                 background_normal='',
+                                 color= 'black',
+                                 background_color = 'lavenderblush')
+        self.btn_buscar.bind(on_press=self.buscar)
 
-        btn_reiniciar = Button(text="Reiniciar")
-        btn_reiniciar.background_color = "mediumvioletred"
+        btn_reiniciar = Button(text="Reiniciar",
+                               font_size = self.width / 2,
+                               background_normal='',
+                               color= 'black',
+                               background_color = 'mintcream')
         btn_reiniciar.bind(on_press=self.reiniciar)
 
         button_layout.add_widget(self.spinner)
-        button_layout.add_widget(btn_buscar)
+        button_layout.add_widget(self.btn_buscar)
         button_layout.add_widget(btn_reiniciar)
 
         self.add_widget(button_layout)
 
     def reiniciar(self, instance):
-        # Reiniciar animaciones
         for button in self.search_widget.buttons:
             Animation.cancel_all(button, 'background_color')
 
-        # Descongelar botones
         for i, button in enumerate(self.search_widget.buttons):
-            if i != 0 and i != 99:
+            if i != 0 and i != CASILLAS ** 2 - 1:
                 button.is_toggeable = True
                 if button.is_toggled:
                     button.toggle()
                 else:
                     button.background_color = button.default_color
 
-    def buscar(self, instance):
-        # Congelar botones
-        for button in self.search_widget.buttons:
-            button.is_toggeable = False
+        self.btn_buscar.disabled = False
+        self.spinner.disabled = False
 
-        # Cancelar animaciones por si acaso
+    def restablecer_grid(self, instance):
         for button in self.search_widget.buttons:
             Animation.cancel_all(button, 'background_color')
+
+        for i, button in enumerate(self.search_widget.buttons):
+            if i == 0:
+                button.background_color = "darkseagreen"
+            elif i == CASILLAS ** 2 - 1:
+                button.background_color = "maroon"
+            else:
+                button.is_toggeable = True
+                if not button.is_toggled:
+                    button.background_color = button.default_color
+
+        self.btn_buscar.disabled = False
+        self.spinner.disabled = False
+
+    def buscar(self, instance):
+        self.btn_buscar.disabled = True
+        self.spinner.disabled = True
+
+        # Congelar botones en grid
+        for button in self.search_widget.buttons:
+            button.is_toggeable = False
 
         # Obtener el valor del spinner
         busqueda_sel = self.spinner.text
@@ -149,21 +180,24 @@ class MainLayout(BoxLayout):
             self.visualize(path)
         else:
             popup = Popup(title=':C',
-                          content=Label(text='No es posible llegar a la meta'),
+                          content=Label(text='No es posible llegar a la meta',
+                                        font_size=self.width / 50),
                           size_hint=(0.3, 0.2))
             popup.open()
+            self.restablecer_grid(self)
 
-    def visualize(self, path, index=0):
+    def visualize(self, path, index=1):  # Brincamos celda inicial
         if not path or index >= len(path):
+            self.restablecer_grid(self)
             return
 
         # Tenemos que usar una lista de colores para poder animar
-        head = self.search_widget.buttons[path[index+1]] # Brincamos celda inicial
-        anim = Animation(background_color=[0, 1, 0, 1], duration=0.1) + \
-               Animation(background_color=[0.7, 1, 0.7, 1], duration=0.1)
+        anim = Animation(background_color=[0, 0.8, 0, 0.5], duration=0.005)
+        head = self.search_widget.buttons[path[index]]
 
         anim.bind(on_complete=lambda *args: self.visualize(path, index+1))
         anim.start(head)
+
 
 class SearchApp(App):
     def build(self):
